649699de76ccebfe3c6500dd5b8ee413693c5649
Laura <laura.barona@uao.edu.co>
2025-10-09 09:57:44 -0500

    setup project

---
+++ src/test/java/com/onclass/bootcamp/BootcampServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclass.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
